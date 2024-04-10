3e5452e867b84c081e7fa03ad7226e9400c95795
Easyfumi <oleg.marinin.49@yandex.ru>
2024-04-10 21:32:03 +0300

    Initial commit

---
+++ Ovito/src/main/java/ru/marinin/ovito/OvitoApplication.java
@@ -0,0 +1,13 @@
+package ru.marinin.Ovito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvitoApplication.class, args);
+	}
+
+}
---
+++ Ovito/src/test/java/ru/marinin/Ovito/OvitoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.marinin.Ovito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
